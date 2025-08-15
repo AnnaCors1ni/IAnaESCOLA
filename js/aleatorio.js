84a15881bf965b5d7d7484f649a4128e160bf787
AnnaCors1ni <147632795+AnnaCors1ni@users.noreply.github.com>
2025-08-15 15:35:24 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,9 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Anna", "Beatriz", "Prof Alessandra", "Marcelo", "Amanda", "Pedro", "Gabriel"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
+
 
 export const nome = aleatorio(nomes)
